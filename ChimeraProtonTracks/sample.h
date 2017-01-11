ade3f2e999f61c215082d181e1c2b0d33cfe77bc
Adrien Hourlier <hourlier@uboonebuild01.fnal.gov>
2017-01-11 08:07:48 -0600

    added new package ChimeraProtonTracks

---
+++ ChimeraProtonTracks/sample.h
@@ -0,0 +1,36 @@
+/**
+ * \file sample.h
+ *
+ * \ingroup ChimeraProtonTracks
+ * 
+ * \brief Class def header for a class sample
+ *
+ * @author hourlier
+ */
+
+/** \addtogroup ChimeraProtonTracks
+
+    @{*/
+#ifndef CHIMERA_ADRIEN_CHIMERAPROTONTRACKS_SAMPLE_H
+#define CHIMERA_ADRIEN_CHIMERAPROTONTRACKS_SAMPLE_H
+
+/**
+   \class sample
+   User defined class ChimeraProtonTracks ... these comments are used to generate
+   doxygen documentation!
+ */
+class sample{
+
+public:
+
+  /// Default constructor
+  sample(){}
+
+  /// Default destructor
+  virtual ~sample(){};
+
+};
+
+#endif
+/** @} */ // end of doxygen group 
+
